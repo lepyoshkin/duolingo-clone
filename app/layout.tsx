21a1f9eb9a85791d44a918f14e568b69d395eb84
lepyoshkin <lepyoshkin.artem@gmail.com>
2024-10-10 16:31:16 +0300

    add lesson header

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import {Toaster} from '@/components/ui/sonner';
 
 import {ClerkProvider} from '@clerk/nextjs';
+import {ExitModal} from '@/components/modals/exit-modal';
 const font = Nunito({subsets: ['latin']});
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={font.className}>
           <Toaster />
+          <ExitModal />
           {children}
         </body>
       </html>
